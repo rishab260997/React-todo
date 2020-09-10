3cd4316fc332157574aec770cf987c7b6e26213c
Rishab260997 <rishab260997@gmail.com>
2020-09-10 12:03:24 +0530

    Updates

---
+++ precache-manifest.1bcaa02cdf5efdee9137d0c611d0a72f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7867996bd6b76c5693b3f4da6ed532d6",
+    "url": "/React-todo/index.html"
+  },
+  {
+    "revision": "58d35e9bbceb0cc950de",
+    "url": "/React-todo/static/css/main.b3c9b034.chunk.css"
+  },
+  {
+    "revision": "1979673bd7cb7101276a",
+    "url": "/React-todo/static/js/2.fa2ca534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo/static/js/2.fa2ca534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d35e9bbceb0cc950de",
+    "url": "/React-todo/static/js/main.e8516fcc.chunk.js"
+  },
+  {
+    "revision": "7ae1248ba8be12ae26ac",
+    "url": "/React-todo/static/js/runtime-main.bfdd67f1.js"
+  }
+]);
